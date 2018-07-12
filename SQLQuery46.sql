3470c6e3fe6af663f0f9c67d64f15eea572f8dd9
Toad <172bandicoots@gmail.com>
2018-07-11 21:22:29 -0400

    tr

---
+++ SQLQuery46.sql
@@ -0,0 +1,15 @@
+﻿USE [aspnet-TimeSheet2017-20160730103016]
+GO
+
+DECLARE	@return_value Int,
+		@NAME varchar(50)
+
+EXEC	@return_value = [dbo].[sp_GetAssociateName]
+		@UserIdent = N'Associate@Timesheet2017.com',
+		@NAME = @NAME OUTPUT
+
+SELECT	@NAME as N'@NAME'
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ getAssociateName.sql
@@ -0,0 +1,11 @@
+﻿USE [aspnet-TimeSheet2017-20160730103016]
+GO
+
+DECLARE	@return_value VarChar(50)
+
+EXEC	@return_value = [dbo].[GetAssociateName]
+		@UserIdent = N'Manager@Timesheet2017.com'
+
+SELECT	@return_value as 'Return Value'
+
+GO
